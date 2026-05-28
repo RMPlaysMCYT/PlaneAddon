60c1dc6993efeebf3b7ed8ce7465e2e1cf4a8489
RMPlaysMC YT <76448296+RMPlaysMCYT@users.noreply.github.com>
2026-05-28 12:07:23 +0800

    add changes

--- packs/BP/scripts/PlaneRonnelAddon/main.js
+++ packs/BP/scripts/PlaneRonnelAddon/main.js
@@ -4,11 +4,11 @@ import './camera/camera';
 console.warn("Plane Add-on Version 8 is Loaded");
 console.log("Plane Add-on Version 8 is Loaded");
 
-world.afterEvents.playerJoin.subscribe((event) => {
-    const {player} = event;
-    world.sendMessage('Thank you for downloading the Plane Add-on Version 8')
-    world.sendMessage('Add-on Developed by RMPlaysMC YT')
-});
+// world.afterEvents.playerJoin.subscribe((event) => {
+//     const {player} = event;
+//     world.sendMessage('Thank you for downloading the Plane Add-on Version 8')
+//     world.sendMessage('Add-on Developed by RMPlaysMC YT')
+// });
 
 world.afterEvents.playerSpawn.subscribe((event) => {
     const {player} = event;
